f4610de2ef41886741d8cdbb80290dff8eb49e62
Trai Sawadphol <traisawadphol@gmail.com>
2023-02-28 17:14:25 +0700

    Update Exercise4_Trai_S.py

---
+++ Exercise4_Trai_S.py
@@ -0,0 +1,10 @@
+'''Year 2 Students Semester 1'''
+eng = 80.00
+bus = 80.00
+comsys = 80.00
+compro = 80.00
+print("---Your Score---")
+print("Foundation English :",eng)
+print("General Business :",bus)
+print("Introduction to Computer Systems :",comsys)
+print("Computer Programming", compro)
