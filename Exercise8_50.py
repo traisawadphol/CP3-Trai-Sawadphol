c6c182dc4fc3e56e79125194636d66d1f8dea28a
Trai Sawadphol <traisawadphol@gmail.com>
2023-03-02 21:06:52 +0700

    Create Exercise8_50.py

---
+++ Exercise8_50.py
@@ -0,0 +1,13 @@
+def addition(x, y):
+    print("x+y =", x+y)
+def subtraction(x, y):
+    print("x-y =", x-y)
+def multilication(x, y):
+    print("x*y =", x*y)
+def division(x, y):
+    print("x/y =", x/y)
+
+addition(5,10)
+subtraction(5,10)
+multilication(5,10)
+division(5,10)
